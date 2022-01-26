984178aa360e66a830f9793fdbf1d38968bd5bfe
Neha Gupta <gneha21@yahoo.in>
2022-01-26 20:25:58 +0530

    refactor: window null pointer check

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,8 @@ import { createUploadLink } from "apollo-upload-client"
 import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client"
 import { BodyType, Method, TestCase, TestQuery, TestStatus } from "./services/queries"
 
-console.log(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/`)
 const fetch = require("node-fetch")
-export const SERVER_URL = (process.env.GATSBY_API_URL == "" || process.env.GATSBY_API_URL == undefined)? `${window.location.protocol}://${window.location.hostname}:${window.location.port}/api/` : `${process.env.GATSBY_API_URL}/`
+export const SERVER_URL = ((process.env.GATSBY_API_URL == "" || process.env.GATSBY_API_URL == undefined) && typeof window !== 'undefined')? `${window.location.protocol}://${window.location.hostname}:${window.location.port}/api/` : `${process.env.GATSBY_API_URL}/`
 export const HTTP_LINK = createUploadLink({ uri: `query`, fetch: fetch })
 export const AUTH_LINK = new ApolloLink((operation, forward) => {
   // Use the setContext method to set the HTTP headers.

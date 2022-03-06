1e3d1fd6c413bb7f7a530a9fcb68b359cd971c97
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2022-03-07 01:34:24 +0530

    fix(testrun-detail): Close test case detail when clicked on tab. (#27)
    now user can click on tab buttons on a particular test run case and go back to default view

--- src/components/testrun/testrun-detail.tsx
+++ src/components/testrun/testrun-detail.tsx
@@ -12,7 +12,8 @@ import Loading from "../global/backdrop"
 import ErrorView from "../global/error"
 import { useQuery } from "@apollo/client"
 import { a11yProps, CustomTab, TabPanelBox } from "../global/tab-panel"
-
+import { defaultTq } from "../../constants"
+import { TestQuery } from "../../services/queries"
 export interface TestRunDetailProps {
   testRunID: string
 }
@@ -39,11 +40,12 @@ const useStyles = makeStyles(() => ({
 export default function TestRunDetail(props: TestRunDetailProps) {
   const classes = useStyles()
   const [value, setValue] = React.useState(0)
+  const [testDetail, setTestDetail] = React.useState<TestQuery>(defaultTq)
   const { loading, error, data } = useQuery<TestRunData>(GET_TEST_RUN_DETAIL, { variables: { id: props.testRunID } })
   if (loading) return (<Loading />)
   if (error) return <ErrorView msg={error.message} />
   if (data == undefined || data?.testRun == undefined || data?.testRun[0].tests == undefined || data?.testRun[0].tests?.length == 0) {
-    return (<Empty doc={"https://github.com/keploy/keploy"} message={"Please wait while we run test cases for you! "} image={EmptyImg}/>)
+    return (<Empty doc={"https://github.com/keploy/keploy"} message={"Please wait while we run test cases for you! "} image={EmptyImg} />)
   }
 
   let urlData = getTestForURL(data.testRun[0].tests)
@@ -67,17 +69,17 @@ export default function TestRunDetail(props: TestRunDetailProps) {
               {[...urlData.keys()].map((e, i) => (
                 <CustomTab
                   key={e} label={<React.Fragment>
-                  <Grid container direction={"column"}>
-                    <Grid item container>
-                      <Grid item xs={6}> <Typography> {urlData.get(e)![0].req.method}</Typography></Grid>
-                      <Grid item xs={6} container justifyContent={"flex-end"}>
-                        <Typography className={classes.caption}>{urlData.get(e)!.length} tests</Typography></Grid>
+                    <Grid container direction={"column"} onClick={() => setTestDetail(defaultTq)}>
+                      <Grid item container>
+                        <Grid item xs={6}> <Typography> {urlData.get(e)![0].req.method}</Typography></Grid>
+                        <Grid item xs={6} container justifyContent={"flex-end"}>
+                          <Typography className={classes.caption}>{urlData.get(e)!.length} tests</Typography></Grid>
+                      </Grid>
+                      <Grid item>
+                        <Typography className={classes.url}>{urlData.get(e)![0].uri}</Typography>
+                      </Grid>
                     </Grid>
-                    <Grid item>
-                      <Typography className={classes.url}>{urlData.get(e)![0].uri}</Typography>
-                    </Grid>
-                  </Grid>
-                </React.Fragment>} {...a11yProps(i)} />
+                  </React.Fragment>} {...a11yProps(i)} />
               ))}
             </Tabs>
           </Grid>
@@ -116,7 +118,7 @@ export default function TestRunDetail(props: TestRunDetailProps) {
           </Card>
           {[...urlData.keys()].map((k, i) => (
             <TabPanelBox key={k} value={value} index={i}>
-              <TestTab tests={urlData.get(k)!} editMode={false} />
+              <TestTab tests={urlData.get(k)!} editMode={false} setTestDetail={setTestDetail} testDetail={testDetail} />
             </TabPanelBox>
           ))}
         </Grid>

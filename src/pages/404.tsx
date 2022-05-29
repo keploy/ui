49454ddf8901f814cbd8cab70c30b27d4197eea8
khanjasir90 <khanmohdjasir@gmail.com>
2022-05-29 08:09:45 +0530

    fixed redirect to doc link issue
    Signed-off-by: khanjasir90 <khanmohdjasir@gmail.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,6 +5,6 @@ import EmptyImg from "../../static/empty3.png"
 
 export default function NotFound() {
   return (
-    <Empty doc={"https://github.com/keploy/keploy"} message={"Whops! This is not the page you're looking for. "} image={EmptyImg}/>
+    <Empty doc={"https://docs.keploy.io/"} message={"Whops! This is not the page you're looking for. "} image={EmptyImg}/>
   )
 }

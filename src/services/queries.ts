ba8b3e44a72ab1f06acee9e06962ad8eca05e501
iamskp99 <iamskp99@gmail.com>
2022-08-09 14:32:05 +0530

    Add query for IDs of all testcases
    Signed-off-by: iamskp99 <iamskp99@gmail.com>

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -206,6 +206,13 @@ query getTc ($app : String!,$offset : Int!, $limit : Int!) {
   }
 }
 `
+export const GET_TOTAL_TC = gql`
+query getTc ($app : String!,$offset : Int!, $limit : Int!){
+  testCase(app: $app,offset: $offset,limit :$limit) {
+          id
+  }
+}
+`
 
 export const GET_APP_TC = gql`
 query getTc ($app : String!) {
@@ -253,6 +260,8 @@ query getTc ($app : String!) {
   }
 }
 `
+
+
 
 export const NORMALISE_TC = gql`
   mutation normalizeTests( $ids: [String!]!) {

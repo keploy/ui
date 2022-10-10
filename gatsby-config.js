9b078706bd624c1c05a204e27fed702be6f05873
sreekesh93 <sreekeshsbkv@gmail.com>
2022-10-10 18:37:38 +0530

    add path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/keploy`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
